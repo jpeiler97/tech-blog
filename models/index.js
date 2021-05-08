3fd577c9bc473008109acec87de08661fa93f7af
JP Eiler <jp.eiler97@gmail.com>
2021-05-08 14:01:21 -0500

    added models

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+	foreignKey: 'userId'
+});
+
+User.hasMany(Post, {
+	foreignKey: 'userId'
+});
+
+Comment.belongsTo(User, {
+	foreignKey: 'userId',
+	onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+	foreignKey: 'postId',
+	onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+	foreignKey: 'postId',
+	onDelete: 'CASCADE'
+});
+
+module.exports = { User, Project, Comment };
