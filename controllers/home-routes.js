4dcde50f160d564b3a77fa056bacabcce50032b9
JP Eiler <jp.eiler97@gmail.com>
2021-05-10 13:45:10 -0500

    added routes for other views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,25 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
 	try {
-		res.render('test');
+		res.render('homepage');
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+router.get('/login', async (req, res) => {
+	try {
+		res.render('login');
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+router.get('/dashboard', async (req, res) => {
+	try {
+		res.render('dashboard');
 	} catch (err) {
 		console.log(err);
 		res.status(500).json(err);
