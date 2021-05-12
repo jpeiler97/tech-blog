61211fb8e5a733b430504c3f8a451705b31fe838
JP Eiler <jp.eiler97@gmail.com>
2021-05-12 13:40:18 -0500

    added user-routes file and sessions to server file

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+const sess = {
+	secret: process.env.SECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
