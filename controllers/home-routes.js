c20b9d1518371a504d5f9a1bde52aed5a32c791f
JP Eiler <jp.eiler97@gmail.com>
2021-05-11 20:30:39 -0500

    added user name to post titles on home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 
 router.get('/', async (req, res) => {
 	try {
-		const postData = await Post.findAll();
+		const postData = await Post.findAll({
+			include: [
+				{
+					model: User,
+					attributes: [ 'name' ]
+				}
+			]
+		});
 
 		const posts = postData.map((post) => post.get({ plain: true }));
 		res.render('homepage', { posts });
