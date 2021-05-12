a7ff01cd6d57462b55c142da9a286fd4be93d282
JP Eiler <jp.eiler97@gmail.com>
2021-05-11 20:17:19 -0500

    added time format helper and displayed post titles on home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+const { Post } = require('../models');
 
 router.get('/', async (req, res) => {
 	try {
-		res.render('homepage');
+		const postData = await Post.findAll();
+
+		const posts = postData.map((post) => post.get({ plain: true }));
+		res.render('homepage', { posts });
 	} catch (err) {
 		console.log(err);
 		res.status(500).json(err);
