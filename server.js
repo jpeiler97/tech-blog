61609321d051da676b4f44bbb4cf13d094bb503d
JP Eiler <jp.eiler97@gmail.com>
2021-05-14 22:42:08 -0500

    changed session expiration time

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
 	secret: process.env.SECRET,
-	cookie: {},
+	cookie: { maxAge: 600000 },
 	resave: false,
 	saveUninitialized: true,
 	store: new SequelizeStore({
