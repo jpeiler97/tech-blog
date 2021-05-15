9c05d2bb06ef807a5c07acd4885934efce8fd994
JP Eiler <jp.eiler97@gmail.com>
2021-05-14 23:48:02 -0500

    changed server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
 	secret: 'superdupersecret',
