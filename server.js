def4ead1025bc63c8a3989dbcfe8e5868400b545
JP Eiler <jp.eiler97@gmail.com>
2021-05-14 23:34:19 -0500

    changed server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-	secret: process.env.SECRET,
+	secret: 'superdupersecret',
 	cookie: { maxAge: 600000 },
 	resave: false,
 	saveUninitialized: true,
